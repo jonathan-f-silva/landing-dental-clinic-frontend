f66d5fd271d1300ccd05256116230a60897703de
Jonathan Ferreira <j0n4t@hotmail.com>
2023-08-27 11:44:07 -0300

    feat: adiciona imagem dentista

--- src/components/MainLanding.tsx
+++ src/components/MainLanding.tsx
@@ -1,10 +1,11 @@
 import { Button, Image, Stack, Text, VStack } from "@chakra-ui/react";
+import dentistImage from "../assets/dentist-sample.png";
 
 function MainLanding() {
   return (
     <Stack as="main" direction={{ base: "column", md: "row" }}>
       {/* TODO: Change Placeholder image */}
-      <Image src="https://plchldr.co/i/300" rounded={10} />
+      <Image src={dentistImage} rounded={10} />
       <VStack>
         <Text padding={10}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
