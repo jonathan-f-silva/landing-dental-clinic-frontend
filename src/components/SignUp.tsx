69431de55f19fec1e4f5423f852f066ce8daedbe
Jonathan Ferreira <j0n4t@hotmail.com>
2023-08-28 17:33:32 -0300

    test: SignUp

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,17 +1,40 @@
 import {
   Button,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
+  FormControl,
+  FormLabel,
   Heading,
   Input,
   Stack,
 } from "@chakra-ui/react";
+import { Card, CardBody, CardFooter, CardHeader } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  name: string;
+  email: string;
+};
 
 function SignUp() {
+  const { register, handleSubmit, formState } = useForm<FormData>({
+    mode: "onChange",
+  });
+
+  const onSubmit = (data: FormData) => {
+    // TODO: send data to server
+    console.log(data);
+  };
+
+  const isSubmitDisabled = !formState.isValid;
+
   return (
-    <Card margin={4} maxWidth="60ch" rounded={10} width="90%">
+    <Card
+      as="form"
+      margin={4}
+      maxWidth="60ch"
+      rounded={10}
+      width="90%"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <CardHeader>
         <Heading color="teal" as={"h3"} size="lg">
           Cadastre-se
@@ -19,12 +42,28 @@ function SignUp() {
       </CardHeader>
       <CardBody>
         <Stack>
-          <Input placeholder="Nome" />
-          <Input placeholder="E-mail" />
+          <FormControl>
+            <FormLabel>Nome</FormLabel>
+            <Input
+              type="text"
+              placeholder="Nome"
+              {...register("name", { required: true })}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>E-mail</FormLabel>
+            <Input
+              type="email"
+              placeholder="E-mail"
+              {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+            />
+          </FormControl>
         </Stack>
       </CardBody>
       <CardFooter>
-        <Button>Cadastrar</Button>
+        <Button isDisabled={isSubmitDisabled} type="submit" colorScheme="teal">
+          Cadastrar
+        </Button>
       </CardFooter>
     </Card>
   );
