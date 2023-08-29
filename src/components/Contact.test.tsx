1da9876081256f026127216077734c0b6fc28916
Jonathan Ferreira <j0n4t@hotmail.com>
2023-08-29 10:21:17 -0300

    test: Contact

---
+++ src/components/Contact.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen, userEvent } from "../utils/test-utils";
+import Contact from "./Contact";
+
+test("renders contact component", () => {
+  render(<Contact />);
+  const title = screen.getByText(/Entre em contato!/i);
+  expect(title).toBeInTheDocument();
+  const name = screen.getByLabelText(/Nome/i);
+  expect(name).toBeInTheDocument();
+  const email = screen.getByLabelText(/E-mail/i);
+  expect(email).toBeInTheDocument();
+  const phone = screen.getByLabelText(/Telefone/i);
+  expect(phone).toBeInTheDocument();
+  const message = screen.getByLabelText(/Mensagem/i);
+  expect(message).toBeInTheDocument();
+  const submitButton = screen.getByRole("button", { name: /Enviar/i });
+  expect(submitButton).toBeDisabled();
+});
+
+test("submits form only with valid data", async () => {
+  render(<Contact />);
+  const name = screen.getByLabelText(/Nome/i);
+  const email = screen.getByLabelText(/E-mail/i);
+  const phone = screen.getByLabelText(/Telefone/i);
+  const message = screen.getByLabelText(/Mensagem/i);
+  const submitButton = screen.getByRole("button", { name: /Enviar/i });
+
+  await userEvent.type(name, "John Doe");
+  await userEvent.type(email, "jonh.doe@example.com");
+  await userEvent.type(phone, "11999999999");
+  await userEvent.type(message, "Olá, gostaria de um orçamento!");
+
+  expect(name).toHaveValue("John Doe");
+  expect(email).toHaveValue("jonh.doe@example.com");
+  expect(phone).toHaveValue("11999999999");
+  expect(message).toHaveValue("Olá, gostaria de um orçamento!");
+  expect(submitButton).toBeEnabled();
+});
