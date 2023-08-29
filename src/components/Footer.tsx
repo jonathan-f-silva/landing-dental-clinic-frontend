6e4b2efb48253f9cccd1240c10880b342cf20fde
Jonathan Ferreira <j0n4t@hotmail.com>
2023-08-29 16:43:11 -0300

    fix: Footer - deixa cor original

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,8 @@
-import { Stack, Text, useColorModeValue } from "@chakra-ui/react";
+import { Stack, Text } from "@chakra-ui/react";
 
 function Footer() {
   return (
-    <Stack
-      as="footer"
-      backgroundColor={useColorModeValue("gray.50", "gray.900")}
-      color={useColorModeValue("gray.700", "gray.200")}
-      padding={4}
-      width="100%"
-    >
+    <Stack as="footer" padding={4} width="100%">
       <Text>© 2023 Vai De Digital. Todos os direitos reservados</Text>
     </Stack>
   );
