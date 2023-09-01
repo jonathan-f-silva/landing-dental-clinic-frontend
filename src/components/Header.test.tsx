5de6d6b5d992ca61f6157804f1dd6e9e6d5fefc5
Jonathan Ferreira <j0n4t@hotmail.com>
2023-09-01 11:00:38 -0300

    refactor: Header test

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -1,32 +1,22 @@
 import { render, screen } from "../utils/test-utils";
 import Header from "./Header";
 
-test("renders header with correct title and links", () => {
+test("renders header with correct title", () => {
   render(<Header title="Clínica Odontológica" />);
   const title = screen.getByText(/Clínica Odontológica/i);
-  const contactLink = screen.getByRole("link", { name: /Contato/i });
-  const registerLink = screen.getByRole("link", { name: /Localização/i });
   expect(title).toBeInTheDocument();
-  expect(contactLink).toBeInTheDocument();
-  expect(registerLink).toBeInTheDocument();
 });
 
-test("renders header with additional links", () => {
-  const additionalLinks = [
+test("renders header with links", () => {
+  const links = [
     {
       href: "#about",
       description: "Sobre",
     },
   ];
-  render(
-    <Header title="Clínica Odontológica" additionalLinks={additionalLinks} />,
-  );
+  render(<Header title="Clínica Odontológica" links={links} />);
   const title = screen.getByText(/Clínica Odontológica/i);
-  const contactLink = screen.getByRole("link", { name: /Contato/i });
-  const registerLink = screen.getByRole("link", { name: /Localização/i });
   const aboutLink = screen.getByRole("link", { name: /Sobre/i });
   expect(title).toBeInTheDocument();
-  expect(contactLink).toBeInTheDocument();
-  expect(registerLink).toBeInTheDocument();
   expect(aboutLink).toBeInTheDocument();
 });
