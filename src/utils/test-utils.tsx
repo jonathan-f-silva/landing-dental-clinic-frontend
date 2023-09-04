9268feaea77750bdeb9b42407ee3abdeea98099c
Jonathan Ferreira <j0n4t@hotmail.com>
2023-09-04 12:37:27 -0300

    test: add context and router

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,20 +1,53 @@
+/* eslint-disable react-refresh/only-export-components */
 import { cleanup, render } from "@testing-library/react";
 import { afterEach } from "vitest";
+import {
+  RouteObject,
+  RouterProvider,
+  createMemoryRouter,
+} from "react-router-dom";
+import { ConfigProvider } from "../contexts/ConfigProvider";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../theme";
+import appRoutes from "../routes";
 
 afterEach(() => {
   cleanup();
 });
 
+const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ConfigProvider>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+    </ConfigProvider>
+  );
+};
+
 function customRender(ui: React.ReactElement, options = {}) {
   return render(ui, {
-    // wrap provider(s) here if needed
-    wrapper: ({ children }) => children,
+    wrapper: AllTheProviders,
     ...options,
   });
 }
 
-// eslint-disable-next-line react-refresh/only-export-components
+/**
+ * Render isolated element with router
+ * @param initialEntries (optional) Initial entries for router (default: ["/"])
+ * @param routes (optional) Routes for router (default: appRoutes)
+ * @returns Rendered element with router
+ */
+function renderWithRouter(
+  initialEntries = ["/"],
+  routes: RouteObject[] = appRoutes,
+) {
+  return render(
+    <AllTheProviders>
+      <RouterProvider router={createMemoryRouter(routes, { initialEntries })} />
+    </AllTheProviders>,
+  );
+}
+
 export * from "@testing-library/react";
 export { default as userEvent } from "@testing-library/user-event";
 // override render export
-export { customRender as render };
+export { customRender as render, renderWithRouter };
