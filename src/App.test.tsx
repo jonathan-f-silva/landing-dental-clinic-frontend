9a65da9a62f1ccdfbae8697cf36f83929050a41e
Jonathan Ferreira <j0n4t@hotmail.com>
2023-09-04 12:37:58 -0300

    test: mvp dashboard test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,19 @@
-import { render, screen } from "./utils/test-utils";
-import App from "./App";
+import { renderWithRouter, screen } from "./utils/test-utils";
 
 test("renders main application components", () => {
-  render(<App />);
+  renderWithRouter();
   screen.getByText(/Clínica Odontológica/i);
   screen.getByText(/Onde estamos/i);
   screen.getByText(/Entre em contato!/i);
   screen.getByText(/Rua 15 de Novembro, 1234/i);
   screen.getByText(/vai de digital/i);
+});
+
+test("renders dashboard components", () => {
+  renderWithRouter(["/dashboard"]);
+  screen.getByText(/Dashboard/i);
+  screen.getByText(/Introdução/i);
+  screen.getByText(/Banner/i);
+  screen.getByText(/Adicionar botões para as seções/i);
+  screen.getByText(/Salvar alterações/i);
 });
