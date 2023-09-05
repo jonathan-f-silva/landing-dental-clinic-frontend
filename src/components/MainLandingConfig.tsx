113d99e1c164932134d28255aac56ec97b2d85d3
Jonathan Ferreira <j0n4t@hotmail.com>
2023-09-05 12:22:21 -0300

    feat: MainLandingConfig usando Context

--- src/components/MainLandingConfig.tsx
+++ src/components/MainLandingConfig.tsx
@@ -9,8 +9,12 @@ import {
   Switch,
   Textarea,
 } from "@chakra-ui/react";
+import { useConfig, useConfigDispatch } from "../contexts/ConfigContext";
 
 function MainLandingConfig() {
+  const { mainLanding } = useConfig();
+  const dispatch = useConfigDispatch();
+
   return (
     <Card>
       <CardHeader>
@@ -19,7 +23,16 @@ function MainLandingConfig() {
       <CardBody>
         <FormControl>
           <FormLabel>Introdução</FormLabel>
-          <Textarea placeholder="Texto de introdução" />
+          <Textarea
+            placeholder="Texto de introdução"
+            value={mainLanding.introText}
+            onChange={(ev) =>
+              dispatch({
+                type: "setMainLanding",
+                payload: { introText: ev.target.value },
+              })
+            }
+          />
         </FormControl>
         <FormControl>
           <FormLabel>Banner</FormLabel>
@@ -27,7 +40,16 @@ function MainLandingConfig() {
         </FormControl>
         <FormControl display="flex" alignItems="center">
           <FormLabel mb="0">Adicionar botões para as seções</FormLabel>
-          <Switch />
+          <Switch
+            isChecked={mainLanding.addSectionButtons}
+            onChange={(ev) => {
+              console.log(ev.target.checked);
+              dispatch({
+                type: "setMainLanding",
+                payload: { addSectionButtons: ev.target.checked },
+              });
+            }}
+          />
         </FormControl>
         <FormControl></FormControl>
       </CardBody>
