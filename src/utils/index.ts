99b0c8efb2db58104b122fda47d1f6467d2c055c
Jonathan Ferreira <j0n4t@hotmail.com>
2023-09-12 14:30:02 -0300

    docs: map embed utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,17 @@
+/**
+ * Checks if a string is a valid google maps embed
+ * @param mapEmbed The google maps embed (iframe code)
+ * @returns Whether the embed is valid or not
+ */ 
 export function isValidEmbed(mapEmbed: string) {
   return mapEmbed.startsWith('<iframe src="https://www.google.com/maps/embed?pb') && mapEmbed.endsWith('</iframe>')
 }
 
+/**
+ * Returns the src from a valid google maps embed
+ * @param mapEmbed The google maps embed (iframe code)
+ * @returns The src URL of the embed
+ */
 export function parseSrcFromEmbed(mapEmbed: string) {
   const srcRegex = /src="([^"]+)"/;
   const src = mapEmbed.match(srcRegex);
