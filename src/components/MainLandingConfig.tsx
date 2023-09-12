47bfe6c2ce10b4ba8a05e7d4b87b310403b926eb
Jonathan Ferreira <j0n4t@hotmail.com>
2023-09-12 15:01:05 -0300

    feat: gera data url image para banner

--- src/components/MainLandingConfig.tsx
+++ src/components/MainLandingConfig.tsx
@@ -5,6 +5,7 @@ import {
   FormControl,
   FormLabel,
   Heading,
+  Image,
   Input,
   Switch,
   Textarea,
@@ -36,14 +37,34 @@ function MainLandingConfig() {
         </FormControl>
         <FormControl>
           <FormLabel>Banner</FormLabel>
-          <Input type="file" name="banner" />
+          <Input
+            type="file"
+            name="banner"
+            onChange={(ev) => {
+              if (!ev.target.files) return;
+              const file = ev.target.files[0];
+              if (file) {
+                const reader = new FileReader();
+                reader.onloadend = () => {
+                  dispatch({
+                    type: "setMainLanding",
+                    payload: { bannerImageURL: reader.result as string },
+                  });
+                };
+                reader.readAsDataURL(file);
+              }
+            }}
+          />
+          <Image
+            src={mainLanding.bannerImageURL}
+            alt="Banner principal do site"
+          />
         </FormControl>
         <FormControl display="flex" alignItems="center">
           <FormLabel mb="0">Adicionar botões para as seções</FormLabel>
           <Switch
             isChecked={mainLanding.addSectionButtons}
             onChange={(ev) => {
-              console.log(ev.target.checked);
               dispatch({
                 type: "setMainLanding",
                 payload: { addSectionButtons: ev.target.checked },
