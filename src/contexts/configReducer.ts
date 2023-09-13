cd8871bc6d5b3c03f08edf1179eef5fd2efabea3
Jonathan Ferreira <j0n4t@hotmail.com>
2023-09-13 18:31:39 -0300

    fix: ponto no Footer

--- src/contexts/configReducer.ts
+++ src/contexts/configReducer.ts
@@ -1,4 +1,3 @@
-
 import dentistImage from "../assets/dentist-sample.png";
 
 export type Config = {
@@ -26,11 +25,11 @@ export type Config = {
 export type Action = {
   payload: Partial<Config>;
   type: "UPDATE_CONFIG";
-}
+};
 
 export const initialConfig: Config = {
   footer: {
-    text: "© 2023 Vai De Digital! Todos os direitos reservados",
+    text: "© 2023 Vai De Digital! Todos os direitos reservados.",
   },
   location: {
     url: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29616.581628772714!2d-49.04017319972907!3d-21.893275947826538!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94bf2202ab90eea3%3A0xb2c6adaca91d8c19!2sIacanga%2C%20SP%2C%2017180-000!5e0!3m2!1spt-BR!2sbr!4v1693317059093!5m2!1spt-BR!2sbr",
