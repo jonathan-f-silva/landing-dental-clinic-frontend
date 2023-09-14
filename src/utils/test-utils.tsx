89ca6ce7c7bc6d8c262c7cf5c7e1741979efb2bf
Jonathan Ferreira <j0n4t@hotmail.com>
2023-09-14 13:00:46 -0300

    remove: route integration tests from vitest
    focus vite on unit testing and playwright for integration

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,14 +1,8 @@
 /* eslint-disable react-refresh/only-export-components */
 import { cleanup, render } from "@testing-library/react";
 import { afterEach } from "vitest";
-import {
-  RouteObject,
-  RouterProvider,
-  createMemoryRouter,
-} from "react-router-dom";
 import { ConfigProvider } from "../contexts/ConfigProvider";
 import AppThemeProvider from "../theme/AppThemeProvider";
-import appRoutes from "../routes";
 
 afterEach(() => {
   cleanup();
@@ -29,24 +23,7 @@ function customRender(ui: React.ReactElement, options = {}) {
   });
 }
 
-/**
- * Render isolated element with router
- * @param initialEntries (optional) Initial entries for router (default: ["/"])
- * @param routes (optional) Routes for router (default: appRoutes)
- * @returns Rendered element with router
- */
-function renderWithRouter(
-  initialEntries = ["/"],
-  routes: RouteObject[] = appRoutes,
-) {
-  return render(
-    <AllTheProviders>
-      <RouterProvider router={createMemoryRouter(routes, { initialEntries })} />
-    </AllTheProviders>,
-  );
-}
-
 export * from "@testing-library/react";
 export { default as userEvent } from "@testing-library/user-event";
 // override render export
-export { customRender as render, renderWithRouter };
+export { customRender as render };
